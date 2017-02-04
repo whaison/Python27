92e16e0ae6ef721763ab98be0da1219934551668
DESKTOP-CO3B6G8\whaison <whaison0422@gmail.com>
2017-02-04 22:49:41 +0900

    C:\Windows\system32>mklink python.exe C:\Python27\Scripts\python.exe python.exe <<===>> C:\Python27\Scripts\python.exe のシンボリック リンクが作成されました
    C:\Windows\system32>cmd /k
    C:\Windows\system32>
    C:\Windows\system32>dir
    2017/02/04  22:39    <SYMLINK>      python.exe [C:\Python27\Scripts\python.exe]

--- Scripts/mklink_this_folder_make_python_exe.bat
+++ Scripts/mklink_this_folder_make_python_exe.bat
@@ -12,5 +12,19 @@
 
 ::mklink %make_symbolic_link_file_path% %link_for%
 mklink python.exe C:\Python27\Scripts\python.exe
+
+
+
+:: bat  right click  admin 
+::C:\Windows\system32>mklink python.exe C:\Python27\Scripts\python.exe
+::python.exe <<===>> C:\Python27\Scripts\python.exe のシンボリック リンクが作成されました
+
+::C:\Windows\system32>cmd /k
+::C:\Windows\system32>
+::C:\Windows\system32>dir
+::2017/02/04  22:39    <SYMLINK>      python.exe [C:\Python27\Scripts\python.exe]
+
+
 ::閉じない
+
 cmd /k
